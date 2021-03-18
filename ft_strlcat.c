2cabb8761d1703ded97226116304aed31f0e0c25
rkochhan <rkochhan@student.42sp.org.br>
2021-03-17 21:58:50 -0300

    fixed ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,30 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rkochhan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/23 14:48:23 by rkochhan          #+#    #+#             */
-/*   Updated: 2020/02/03 13:34:09 by rkochhan         ###   ########.fr       */
+/*   Updated: 2021/03/17 21:58:34 by rkochhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t				ft_strlcat(char *dest, const char *src, size_t size)
 {
 	size_t dstlen;
 	size_t srclen;
 	size_t i;
 	size_t j;
 
-	dstlen = ft_strlen(dst);
-	srclen = ft_strlen(src);
-	if (dstsize < dstlen)
-		return (dstsize + srclen);
 	i = 0;
+	dstlen = ft_strlen(dest);
+	srclen = ft_strlen(src);
+	if (size <= dstlen)
+		return (size + srclen);
 	j = dstlen;
-	while (j < dstsize - 1 && src[i])
-		dst[j++] = src[i++];
-	dst[j] = '\0';
+	while (src[i] != '\0' && j != size - 1)
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	dest[j] = '\0';
 	return (dstlen + srclen);
 }
