62427c3760125f42cb04bd0d2e0e79cbe8c70602
rkochhan <rkochhan@student.42sp.org.br>
2021-04-07 10:58:03 -0300

    made up to norme v3

--- ft_strswap.c
+++ ft_strswap.c
@@ -6,7 +6,7 @@
 /*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/04 07:28:31 by rkochhan          #+#    #+#             */
-/*   Updated: 2021/03/04 07:28:35 by rkochhan         ###   ########.fr       */
+/*   Updated: 2021/04/07 10:57:52 by rkochhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_strswap(char **str1, char **str2)
 {
-	char *temp;
+	char	*temp;
 
 	temp = *str1;
 	*str1 = *str2;
