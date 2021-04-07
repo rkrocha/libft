b40148a262826bb780e7d67f62d7005f5f9cec0e
rkochhan <rkochhan@student.42sp.org.br>
2021-04-07 10:45:15 -0300

    made up to norme v3

--- ft_ullitoa_base.c
+++ ft_ullitoa_base.c
@@ -6,11 +6,19 @@
 /*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 10:14:00 by rkochhan          #+#    #+#             */
-/*   Updated: 2021/03/13 11:23:06 by rkochhan         ###   ########.fr       */
+/*   Updated: 2021/04/07 10:44:49 by rkochhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static bool	get_radix(size_t *radix, char *base)
+{
+	*radix = ft_strlen(base);
+	if (*radix < 2)
+		return (false);
+	return (true);
+}
 
 static size_t	ullitoa_len(unsigned long long num, size_t radix)
 {
@@ -25,31 +33,31 @@ static size_t	ullitoa_len(unsigned long long num, size_t radix)
 	return (len);
 }
 
-char			*ft_ullitoa_base(unsigned long long num, char *base, bool sign)
+char	*ft_ullitoa_base(unsigned long long num, char *base, bool sign)
 {
 	char	*str;
 	size_t	radix;
 	size_t	len;
 	size_t	i;
 
-	if (!base || (radix = ft_strlen(base)) < 2)
+	if (!base || !get_radix(&radix, base))
 		return (NULL);
 	len = ullitoa_len(num, radix) + sign;
-	if (!(str = (char *)malloc((len + 1) * sizeof(char))))
+	str = (char *)malloc((len + 1) * sizeof(char));
+	if (!str)
 		return (NULL);
 	str[len] = '\0';
 	i = 0;
 	if (sign)
 	{
-		str[i] = '-';
+		str[0] = '-';
 		i++;
 	}
-	while ((num / radix) > 0 && len > i)
+	while (len > i)
 	{
 		str[len - 1] = base[num % radix];
 		num /= radix;
 		len--;
 	}
-	str[i] = base[num % radix];
 	return (str);
 }
