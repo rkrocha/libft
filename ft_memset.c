cc98b71f8db791aba54629669032f37855dd952d
rkochhan <rkochhan@student.42sp.org.br>
2021-04-07 10:21:24 -0300

    made up to norme v3

--- ft_memset.c
+++ ft_memset.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rkochhan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/22 14:46:09 by rkochhan          #+#    #+#             */
-/*   Updated: 2020/02/02 15:33:25 by rkochhan         ###   ########.fr       */
+/*   Updated: 2021/04/07 10:20:35 by rkochhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void				*ft_memset(void *b, int c, size_t len)
+void	*ft_memset(void *b, int c, size_t len)
 {
 	unsigned char	*ptr;
 	unsigned char	chr;
